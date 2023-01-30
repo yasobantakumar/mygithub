b60ddd96c46d153b4df607b7863e64000e8a6541
yashb <yashb@yash-pc>
2023-01-31 01:18:49 +0530

    first commit

---
+++ githubprctice/src/githubprctice/Github.java
@@ -0,0 +1,8 @@
+package githubprctice;
+
+public class Github {
+
+	public static void main(String[] args) {
+		System.out.println("this is github");
+	}
+}
---
+++ githubprctice/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yashb
+ *
+ */
+module githubprctice {
+}
